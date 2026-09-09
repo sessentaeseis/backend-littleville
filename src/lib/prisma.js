7ef1ae44439fa462bd76520058b7b93509b0d987
sessentaeseis <66entreaspasduplas@gmail.com>
2026-09-09 00:17:42 -0300

    teste com js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,11 +1,11 @@
-import "dotenv/config"
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../../generated/prisma/client.ts"
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-})
+});
 
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter });
 
-export default prisma
+export default prisma;
